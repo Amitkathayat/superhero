adb2ccb733b29ad85935f68d110abc880d5903b2
Amit Singh Kathayat <53523117+Amitkathayat@users.noreply.github.com>
2022-08-17 21:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,4 @@ async def predict_api(ip:str):
 
 
 if __name__ == '__main__':
-    main()
-#     uvicorn.run(app, host='127.0.0.1', port=5000, debug=True)
+    uvicorn.run(app, host='127.0.0.1', port=5000, debug=True)
